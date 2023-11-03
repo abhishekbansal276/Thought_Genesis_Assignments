0676f514ac7726cc034b689b5f6e9dde3370829a
Abhishek Bansal <73697686+abhishekbansal276@users.noreply.github.com>
2023-11-03 23:58:29 +0530

    Remove extra code

--- Calculator/src/test/scala/CalculatorTestClass.scala
+++ Calculator/src/test/scala/CalculatorTestClass.scala
@@ -17,20 +17,4 @@ class CalculatorTestClass extends AnyFunSuite{
     assert(calculation_object.calculate(10, 5, '/') == 0.0)
     assert(calculation_object.calculate(10, 5, '%') == 2.0)
   }
-
-//  test("Subtraction ------") {
-//    assert(calculation_object.calculate(10, 5, '-') == 5.0)
-//  }
-//
-//  test("Multiplication ------") {
-//    assert(calculation_object.calculate(10, 5, '*') == 50.0)
-//  }
-//
-//  test("Division ------") {
-//    assert(calculation_object.calculate(10, 5, '/') == 2.0)
-//  }
-//
-//  test("Modulus ------") {
-//    assert(calculation_object.calculate(10, 5, '%') == 0.0)
-//  }
 }
